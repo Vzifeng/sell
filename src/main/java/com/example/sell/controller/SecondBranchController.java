d1e3a16e2f5bcda5e1b365d350895b1a57b92078
yangyl <1281084456@qq.com>
2018-11-22 16:39:44 +0800

    branch commit2

---
+++ src/main/java/com/example/sell/controller/SecondBranchController.java
@@ -0,0 +1,18 @@
+package com.example.sell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @ Author   ：yangyunlong.
+ * @ Date     ：Created in 16:38 2018/11/22
+ * @Version ： $version$
+ */
+@Controller
+public class SecondBranchController {
+    @RequestMapping(value = "/ddd",method = RequestMethod.GET)
+    public String ddd(){
+        return "sssssssssssss";
+    }
+}
